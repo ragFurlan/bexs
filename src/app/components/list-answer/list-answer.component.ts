f792261bf5431f6a443eabdef58ec5dffe1840fc
R. Gonçalves Furlan <r.goncalves.furlan@avanade.com>
2020-05-03 21:19:28 -0300

    list-answer

--- src/app/components/list-answer/list-answer.component.ts
+++ src/app/components/list-answer/list-answer.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
-import {Observable} from 'rxjs';
-import {map} from 'rxjs/operators';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-list-answer',
@@ -9,14 +9,59 @@ import {map} from 'rxjs/operators';
   styleUrls: ['./list-answer.component.css']
 })
 export class ListAnswerComponent implements OnInit {
-    id: Observable<string>;
+  id: string;
+  listAnswer: any;
 
-  constructor(route: ActivatedRoute) { 
-    this.id = route.params.pipe(map(p => p.id));
+  constructor(route: ActivatedRoute) {
+    const idObservable = route.params.pipe(map(p => p.id));
+    idObservable.subscribe(e => 
+      {
+        this.listAnswer = this.mockAnswer().filter(function (el) {
+          return el.questionId.indexOf(e) > -1;
+        });
+      })
    
+
+    console.log(this.listAnswer.count);
+
   }
 
   ngOnInit(): void {
+
   }
 
+  mockAnswer() {
+    return [
+      {
+        answer: "Roberta Furlan",
+        questionId: "1",
+        id: "1"
+      },
+      {
+        answer: "O segredo de quem tem.",
+        questionId: "2",
+        id: "2"
+      },
+      {
+        answer: "A alma da vida",
+        questionId: "2",
+        id: "3"
+      },
+      {
+        answer: "Amanhecer",
+        questionId: "2",
+        id: "4"
+      },
+      {
+        answer: "A Grande Guerra",
+        questionId: "3",
+        id: "5"
+      },
+      {
+        answer: "O nome da rosa",
+        questionId: "3",
+        id: "6"
+      }
+    ];
+  }
 }
--- src/app/components/list-question/list-question.component.ts
+++ src/app/components/list-question/list-question.component.ts
@@ -33,5 +33,4 @@ export class ListQuestionComponent implements OnInit {
       }
     ];
   }
-
 }
